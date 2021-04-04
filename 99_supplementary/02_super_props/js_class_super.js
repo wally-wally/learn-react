1c3c9e057c72715ec0455cd4bcd747b73ae125b7
wally-wally <wallys0213@gmail.com>
2021-04-04 18:34:51 +0900

    210404 | mkdir 99_supplementary

---
+++ 99_supplementary/02_super_props/js_class_super.js
@@ -0,0 +1,21 @@
+class Person {
+	constructor(name) {
+    this.name = name;
+  };
+};
+
+// class를 상속 받아서 구성할 때 super 선언 전 this를 사용하면 아래 코드에서 this.name에 그 어떤 것도 매칭이 되지 않기 때문에 오류가 발생할 수 있다.
+// 그리고 super()가 불러오기 전에 this.greetColleagues()가 실행되면 this.name은 아직 변경되지 않았으므로 논리 구조가 상당히 복잡해지게 된다.
+// 그래서 이러한 단점을 보완하기 위해 javascript 내부적으로 자체적으로 super 키워드 실행 전에 this를 사용하면 오류 문구를 출력하게 된다.
+class PolitePerson extends Person {
+	constructor(name) {
+    super(name);
+    this.greetColleagues();
+  };
+  
+  greetColleagues() {
+    alert(`Hello, ${this.name}!`);
+  };
+};
+
+let p1 = new PolitePerson('wally');
